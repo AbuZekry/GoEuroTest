01d0e89207410fb60cf68c180ee42b49884a1107
AbuZekry <abouzekrys@gmail.com>
2016-09-03 02:07:01 +0200

    Create  GEObject.java
    This is the class that has the data _id, name, type, latitude and longitude

---
+++ src/goeuro/test/model/GEObject.java
@@ -0,0 +1,65 @@
+package goeuro.test.model;
+
+public class GEObject {
+	private String _id = "";
+	private String name = "";
+	private String type = "";
+	private String latitude = "";
+	private String longitude = "";
+	
+	public GEObject() {
+	}
+	
+	public GEObject(String id, String name, String type, String latitude, String longitude) {
+		this._id = id;
+		this.name = name;
+		this.type = type;
+		this.latitude = latitude;
+		this.longitude = longitude;
+	}
+	public GEObject(GEObject object) {
+		this._id = object.getId();
+		this.name = object.getName();
+		this.type = object.getType();
+		this.latitude = object.getLatitude();
+		this.longitude = object.getLongitude();
+	}
+	
+	public String getId() {
+		return _id;
+	}
+	public void setId(String id) {
+		this._id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getLatitude() {
+		return latitude;
+	}
+	public void setLatitude(String latitude) {
+		this.latitude = latitude;
+	}
+	public String getLongitude() {
+		return longitude;
+	}
+	public void setLongitude(String longitude) {
+		this.longitude = longitude;
+	}
+	public void eraseData() {
+		this._id = "";
+		this.name = "";
+		this.type = "";
+		this.latitude = "";
+		this.longitude = "";
+	}
+}
